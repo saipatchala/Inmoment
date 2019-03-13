c0d9ace0b3c9ca72ba61c3da839c75500ce0042e
Sai Patchala <Patchala_Sai_K@cat.com>
2019-03-12 20:01:08 -0500

    Simple form submission

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req,res) => {
+    res.sendfile(__dirname+"/"+"index.html");
+})
+
+app.get('/definition', (req, res) => {
+    res.send('Word Sent is: '+req.query.word);
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
